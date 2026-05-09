441fc706a2deef4309646d710b50ce0e46155bbc
Arjun Bhalla <arjunxb18@gmail.com>
2026-05-09 23:58:25 +0530

    Create monthyl-transactions-I.sql

---
+++ medium/monthyl-transactions-I.sql
@@ -0,0 +1,9 @@
+select
+TO_CHAR(trans_date, 'YYYY-MM') as month,
+country,
+count(id) as trans_count,
+count (case when state='approved' then 1 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by TO_CHAR(trans_date, 'YYYY-MM'),country;
