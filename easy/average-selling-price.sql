7c296614837917f31a7b73533a9bc754b79cb0d3
Arjun Bhalla <arjunxb18@gmail.com>
2026-05-09 19:39:41 +0530

    Create average-selling-price.sql

---
+++ easy/average-selling-price.sql
@@ -0,0 +1,7 @@
+select p.product_id, round(coalesce(sum(p.price * u.units)::numeric/sum(u.units), 0), 2) 
+as average_price from 
+Prices p left join UnitsSold u on
+p.product_id = u.product_id
+where u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
+
